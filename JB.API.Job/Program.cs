141936be0a7d9bcb04a4b2005dc741c8551a1c5c
Tran Thuan Thanh <tranthuanthanh535@outlook.com>
2022-02-12 12:40:01 +0700

    [Update] Docker compose

--- JB.API.Job/Program.cs
+++ JB.API.Job/Program.cs
@@ -1,5 +1,6 @@
 using JB.Job.Services;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
@@ -23,10 +24,15 @@ namespace JB.Job
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.Configure<KestrelServerOptions>(
+                        context.Configuration.GetSection("Kestrel"));
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                })
-            ;
+                    webBuilder.UseKestrel();
+                });
     }
 }

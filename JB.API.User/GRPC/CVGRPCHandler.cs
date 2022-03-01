f734f393c7a2cee3826881ec2280ec9802437339
Tran Thuan Thanh <tranthuanthanh535@outlook.com>
2022-03-01 19:55:26 +0700

    Update CV gRPC

---
+++ JB.API.User/GRPC/CVGRPCHandler.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using JB.gRPC.CV;
+using JB.gRPC.Profile;
+using JB.User.Models.CV;
+using JB.User.Models.Profile;
+using JB.User.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace JB.User.GRPC
+{
+    public class CVGRPCHandler : CVRPC.CVRPCBase
+    {
+        private readonly ICVService _cvService;
+        private readonly IMapper _mapper;
+        public CVGRPCHandler(
+            ICVService cvService,
+            IMapper mapper
+            )
+        {
+            _cvService = cvService;
+            _mapper = mapper;
+        }
+
+        public override async Task<CVResponse> Get(CVRequest request, ServerCallContext context)
+        {
+            CVResponse cvResponse = new CVResponse();
+            Expression<Func<CVModel, bool>> filter = _ => false;
+
+            if (request.Id.Count > 0)
+            {
+                filter = x => request.Id.ToArray().Contains(x.Id);
+            }
+
+            (var status, var cvs) = await _cvService.List(filter, x => x.Id, int.MaxValue, 1, false);
+
+            if (status.IsSuccess)
+            {
+                cvResponse.Cvs.AddRange(_mapper.Map<List<CVModel>, List<gRPC.CV.CV>>(cvs));
+            }
+
+            return cvResponse;
+        }
+    }
+}
